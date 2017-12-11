3ca240db71500d07a5d7fd321aab62a243fec99a
FadhProj <fadhlanputrantodev@gmail.com>
2017-12-11 18:43:21 +0700

    12Dec2017

--- TA/TA/Iimage.cs
+++ TA/TA/Iimage.cs
@@ -9,7 +9,7 @@ namespace TA
 {
     class Iimage
     {
-        private struct Pos
+        public struct Pos
         {
             int m;
             int n;
@@ -19,31 +19,37 @@ namespace TA
                 this.m = m;
                 this.n = n;
             }
+
+            public int M { get => m; set => m = value; }
+            public int N { get => n; set => n = value; }
         }
+        
         private Bitmap img;
         private int blok;
         private Pos[] defBlok;
 
         public Bitmap Img { get => img; }
         public int Blok { get => blok; }
-        private Pos[] DefBlok { get => defBlok; }
+        internal Pos[] DefBlok { get => defBlok; }
 
         public Iimage(Bitmap img)
         {
             this.img = img;
+            blok = ((this.img.Height * this.img.Width) / 9);
+            defBlok = new Pos[blok];
             setDefBlok(img);
         }
 
 
         public void setDefBlok(Bitmap img)
         {
-            blok = 0;
+            //blok = 0;
             for (int i = 0; i < img.Height; i+=3)
             {
                 for (int j = 0; j < img.Width; j+=3)
                 {
                     defBlok[blok] = new Pos(j, i);
-                    blok++;
+                   // blok++;
                 }
             }
         }
--- TA/TA/StreamChipper.cs
+++ TA/TA/StreamChipper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace TA
 {
@@ -50,16 +51,30 @@ namespace TA
         }
 
         //Prosedure Pembangkitan Kunci Random
-        public void PRGA()
+        public void PRGA(ref Iimage img)
         {
             int i = 0;
             int j = 0;
-            for (int n = 0; n < 10; n++) //perlu di ubah!!
+            int key;
+            for (int n = 0; n < img.Blok; n++) //perlu di ubah!!
             {
                 i = (j + 1) % 256;
                 j = (j + S[i]) % 256;
                 swap(i, j);
+                key = S[(S[i] + S[j]) % 256];
                 //XOR blok
+
+            }
+        }
+
+        public void xorBlok(ref Iimage img, int key, int n)
+        { 
+            for (int i = img.DefBlok[n].N; i < img.DefBlok[n].N +3 ; i++)
+            {
+                for (int j = img.DefBlok[n].M; j < img.DefBlok[n].M +3 ; j++)
+                {
+                    img.Img.SetPixel(i, j, Color.FromArgb(img.Img.GetPixel(i, j) ^ key));           
+                }
             }
         }
     }
