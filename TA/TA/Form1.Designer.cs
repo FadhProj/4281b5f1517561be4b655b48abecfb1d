6412d9fed7015ed427f20e34efd84202b2c2ad8e
FadhProj <fadhlanputrantodev@gmail.com>
2017-12-11 20:23:37 +0700

    12Dec2017v2

--- TA/TA/Form1.Designer.cs
+++ TA/TA/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.tbDate = new System.Windows.Forms.TextBox();
+            this.pbLoad = new System.Windows.Forms.PictureBox();
+            this.btOpen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLoad)).BeginInit();
             this.SuspendLayout();
             // 
             // tbDate
@@ -40,14 +43,37 @@
             this.tbDate.TabIndex = 0;
             this.tbDate.TextChanged += new System.EventHandler(this.tbDate_TextChanged);
             // 
+            // pbLoad
+            // 
+            this.pbLoad.Location = new System.Drawing.Point(12, 26);
+            this.pbLoad.Name = "pbLoad";
+            this.pbLoad.Size = new System.Drawing.Size(480, 459);
+            this.pbLoad.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbLoad.TabIndex = 1;
+            this.pbLoad.TabStop = false;
+            // 
+            // btOpen
+            // 
+            this.btOpen.Location = new System.Drawing.Point(391, 491);
+            this.btOpen.Name = "btOpen";
+            this.btOpen.Size = new System.Drawing.Size(101, 23);
+            this.btOpen.TabIndex = 3;
+            this.btOpen.Text = "Open";
+            this.btOpen.UseVisualStyleBackColor = true;
+            this.btOpen.Click += new System.EventHandler(this.btOpen_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(933, 513);
+            this.Controls.Add(this.btOpen);
+            this.Controls.Add(this.pbLoad);
             this.Controls.Add(this.tbDate);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbLoad)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +82,8 @@
         #endregion
 
         private System.Windows.Forms.TextBox tbDate;
+        private System.Windows.Forms.PictureBox pbLoad;
+        private System.Windows.Forms.Button btOpen;
     }
 }
 
--- TA/TA/Form1.cs
+++ TA/TA/Form1.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace TA
 {
     public partial class Form1 : Form
     {
+        DateTime dt;
+        Bitmap g;
         public Form1()
         {
             InitializeComponent();
-            DateTime dt = DateTime.Now;
+            dt = DateTime.Now;
             tbDate.Text = dt.ToString();
             
         }
@@ -23,6 +27,46 @@ namespace TA
         private void tbDate_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = " Image Files(*.jpg;*.bmp;*.jpeg;*.png)|*.jpg;*.bmp;*.jpeg;*.png";
+            if(open.ShowDialog() == DialogResult.OK)
+            {
+                pbLoad.Image = new Bitmap(open.FileName);
+                g = (Bitmap) pbLoad.Image;
+            }
+
+            // menampilkan value perpixel
+            BitmapData data = g.LockBits(new Rectangle(0, 0, g.Width, g.Height), ImageLockMode.ReadWrite, g.PixelFormat);
+            unsafe
+            {
+                Console.WriteLine(Bitmap.GetPixelFormatSize(g.PixelFormat) / 8);
+
+                byte* ptr = (byte*)data.Scan0;
+                for (int j = 0; j < data.Height; j++)
+                {
+                    Console.Write("Baris ke {0} : ", j);
+                    for (int i = 0; i < data.Width; i++)
+                    {
+
+                        Console.Write(" K : {0} ; pix : {1} ", i, ptr[i]);
+                        //Console.Write(ptr[i]);
+
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            g.UnlockBits(data);
+            Console.WriteLine(g.Size);
         }
     }
 }
