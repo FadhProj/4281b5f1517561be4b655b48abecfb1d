7c8b1bc8df292ae7479e8d1e8df070b852668b8c
FadhProj <fadhlanputrantodev@gmail.com>
2017-12-23 05:01:54 +0700

    a

--- TA/TA/Permutation.cs
+++ TA/TA/Permutation.cs
@@ -4,25 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Collections;
+using System.Numerics;
 
 namespace TA
 {
     class Permutation
     {
-        int p, a, d;
+        int p, a;
+        BigInteger z, d;
         public Permutation(ref Iimage image)
         {
             p = primeNumber(image.Blok);
+            Console.WriteLine(p);
 
             for (int i = 0; i < image.Blok; i++)
             {
-                d = (int)Math.Pow(5, (i + 69) % (image.Blok + 70) ) % p;
-                Console.WriteLine("i {0} i + 69 = {1} i + 69 % 256 = {2} pow = {3} ", i, i + 69, (i + 69) % (image.Blok + 70), Math.Pow(5, (i + 69) % (image.Blok + 70)));
-                //Console.WriteLine("i {0} d {1} p {2}", i, d,p);
-                //swapBlok(i, d, ref image);
+                z = BigInteger.Pow(5, (i + 69) % (image.Blok + 70));
+                d =  z % p;
+                //Console.WriteLine("i {0} i + 69 = {1} i + 69 % 256 = {2} d = {3} pow = {4} ", i, i + 69, (i + 69) % (image.Blok + 70),d, BigInteger.Pow(5, (i + 69) % (image.Blok + 70)));
+                Console.WriteLine("i {0} d {1} p {2}", i, d,p);
+                swapBlok(i,(int)d, ref image);
             }
         }
 
+        
         public void swapBlok(int blok1,int blok2,ref Iimage image)
         {
             Color tmp;
